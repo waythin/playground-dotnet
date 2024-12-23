5303bca3e304bb7057d485980eed63b8c151a9be
waythin <wathinmarma@gmail.com>
2024-12-24 02:37:35 +0600

    DTO - created a record to explore dto and added some dummy data

--- playgorund.api/Program.cs
+++ playgorund.api/Program.cs
@@ -1,5 +1,16 @@
+using playgorund.api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+
+
+List<EventDto> Events = [
+    new EventDto(1, "Event 1", "Event 1 Description", "https://via.placeholder.com/150", 100, new DateOnly(2022, 1, 1)),
+    new EventDto(2, "Event 2", "Event 2 Description", "https://via.placeholder.com/150", 200, new DateOnly(2022, 2, 2)),
+    new EventDto(3, "Event 3", "Event 3 Description", "https://via.placeholder.com/150", 300, new DateOnly(2022, 3, 3)),
+];
+
 
 app.MapGet("/", () => "Hello World!");
 
