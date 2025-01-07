291d8a48006fd23bc5886f8cbfa4320ceb994675
waythin <wathinmarma@gmail.com>
2025-01-07 09:01:30 +0600

    update 2

--- playgorund.api/Program.cs
+++ playgorund.api/Program.cs
@@ -3,16 +3,10 @@ using playgorund.api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 var ConnString =  builder.Configuration.GetConnectionString("EventStore");
 builder.Services.AddSqlite<EventStoreContext>(ConnString);
 
 var app = builder.Build();
-
-
-
-
 
 // call route endpoints
 app.MapEventEndPoints();
--- playgorund.api/obj/Debug/net9.0/playgorund.api.AssemblyInfo.cs
+++ playgorund.api/obj/Debug/net9.0/playgorund.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("playgorund.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b183ddbe84ca6cb428b0e122314f1ae7c10b25d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf52255c38d006d104c7d6bfbcadcdfcf2f0c69")]
 [assembly: System.Reflection.AssemblyProductAttribute("playgorund.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("playgorund.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
